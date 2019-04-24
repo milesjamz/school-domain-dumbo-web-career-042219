14ab51bfc20df271e3cc84ec55eb56168f981e57
Miles Marillo <mjm03@hampshire.edu>
2019-04-24 17:52:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+
+attr_accessor :name, :roster, :name, :grade
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+  myHash = {}
+  roster << grade
+  grade << name
+  end
+  
+end
